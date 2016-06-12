38f11508c8511dbcf87bf4e4f596ddad67793507
kevinkotowski <kkotowski@8thlight.com>
2016-06-12 12:44:20 -0500

    refactored static to use new class filesystem

---
+++ src/test/java/com/kevinkotowski/server/MockFileSystem.java
@@ -0,0 +1,7 @@
+package com.kevinkotowski.server;
+
+/**
+ * Created by kevinkotowski on 6/11/16.
+ */
+public class MockFileSystem {
+}
---
+++ src/test/java/com/kevinkotowski/server/_FileSystemTest.java
@@ -0,0 +1,31 @@
+package com.kevinkotowski.server;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kevinkotowski on 6/11/16.
+ */
+public class _FileSystemTest {
+    @Test
+    public void isDirectory() throws Exception {
+
+    }
+
+    @Test
+    public void isImage() throws Exception {
+
+    }
+
+    @Test
+    public void isTextFile() throws Exception {
+
+    }
+
+    @Test
+    public void getBytes() throws Exception {
+
+    }
+
+}
